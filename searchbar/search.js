8f30b615939918c95e9f0df33b0a0835581600e4
anirban2703 <anirbanghoshani2703@gmail.com>
2022-01-19 19:45:54 +0530

    searchbar

---
+++ searchbar/search.js
@@ -0,0 +1,10 @@
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('input')
+
+
+btn.addEventListener('click',()=>{
+    //toggle helps to add or remove the class
+    search.classList.toggle('active')
+    input.focus()
+})
